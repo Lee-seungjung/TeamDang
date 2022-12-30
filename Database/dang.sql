059ee02f9e2d90c1e9f0764514e9f57c50907566
Lee-seungjung <109942536+Lee-seungjung@users.noreply.github.com>
2022-12-31 00:29:27 +0900

    Create dang.sql

---
+++ Database/dang.sql
@@ -0,0 +1,18 @@
+-- dang(댕모임)
+
+-- 시퀀스
+create sequence dang_seq;
+
+-- 테이블 생성
+create table dang(
+dang_no number primary key,
+dang_name varchar2(60) not null,
+dang_info varchar2(300) not null,
+dang_headmax number not null,
+dang_head number not null,
+dang_createtime date default sysdate not null,
+dang_like number default 0 not null,
+dang_private char(1) not null,
+dang_pw number,
+district_no references dang_region(district_no) on delete set null
+);
