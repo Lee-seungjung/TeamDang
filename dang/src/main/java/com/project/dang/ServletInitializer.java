6484dcaba5bbbd3572b16ae7a5bf5f8dd1eb34e3
leeseungjung <tmdwjd0295@naver.com>
2022-12-30 16:41:17 +0900

    dang project 생성

---
+++ dang/src/main/java/com/project/dang/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.project.dang;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DangApplication.class);
+	}
+
+}
---
+++ dang/src/test/java/com/project/dang/DangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
