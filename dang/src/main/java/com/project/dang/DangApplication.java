52a5d97bf06f32281431e3e1c24bb438a860c6e2
leeseungjung <tmdwjd0295@naver.com>
2022-12-30 17:31:52 +0900

    dang project 생성

---
+++ dang/src/main/java/com/project/dang/DangApplication.java
@@ -0,0 +1,13 @@
+package com.project.dang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangApplication.class, args);
+	}
+
+}
