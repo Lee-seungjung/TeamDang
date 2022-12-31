ffd15bb1d8bca53e40a35877d847ee551c9fec45
Luverduck <109222367+Luverduck@users.noreply.github.com>
2022-12-31 13:24:23 +0900

    컬럼명 변경

--- Database/dang_region.sql
+++ Database/dang_region.sql
@@ -1,7 +1,7 @@
 --댕모임 지역 테이블 생성 구문
 create table dang_region(
-district_no number PRIMARY KEY,
-district_name varchar2(30) not null,
+dang_area_no number PRIMARY KEY,
+dang_area_name varchar2(30) not null,
 dang_area_x number not null,
 dang_area_y number not null
 );
