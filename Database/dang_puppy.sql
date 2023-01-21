a98945a68b30643f10a0d7af9438992033a42d42
Luverduck <109222367+Luverduck@users.noreply.github.com>
2023-01-21 20:40:28 +0900

    댕댕이 성별에 not null 조건 삭제 및 댕댕이 나이 컬럼 추가

--- Database/dang_puppy.sql
+++ Database/dang_puppy.sql
@@ -3,5 +3,6 @@ create table dang_puppy(
 puppy_no number primary key,
 user_no references dang_user(user_no) on delete cascade,
 puppy_name varchar2(30) not null,
-puppy_gender char(1) not null
+puppy_gender char(1),
+puppy_age number
 );
