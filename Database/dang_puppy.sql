25bb86476bf4b4887be2c27454b3dae01ae036a1
Luverduck <109222367+Luverduck@users.noreply.github.com>
2023-01-25 15:49:27 +0900

    댕댕이 이름 5자로 제한

--- Database/dang_puppy.sql
+++ Database/dang_puppy.sql
@@ -2,7 +2,7 @@
 create table dang_puppy(
 puppy_no number primary key,
 user_no references dang_user(user_no) on delete cascade,
-puppy_name varchar2(30) not null,
+puppy_name varchar2(15) not null,
 puppy_gender char(1),
 puppy_age number
 );
