c7c49a41439dc5bda39f509de3eff0f84b095777
Lee-seungjung <109942536+Lee-seungjung@users.noreply.github.com>
2023-01-27 12:48:34 +0900

    Update dang_report.sql

--- Database/dang_report.sql
+++ Database/dang_report.sql
@@ -6,7 +6,7 @@ create sequence dang_report_seq;
 -- 테이블 등록
 create table dang_report(
 report_no number primary key,
-member_no references dang_member(member_no) on delete cascade,
+user_no references dang_user(user_no) on delete cascade,
 report_content varchar2(300) not null,
 report_date date default sysdate not null,
 report_state char(6) default '접수' check(report_state in('접수','반려','완료')) not null
