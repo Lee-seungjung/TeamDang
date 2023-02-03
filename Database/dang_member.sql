260cef42e043905de5a3a82bb23ebd26ecd32189
Luverduck <109222367+Luverduck@users.noreply.github.com>
2023-02-03 14:59:43 +0900

    댕모임 회원 메시지 컬럼 추가

--- Database/dang_member.sql
+++ Database/dang_member.sql
@@ -9,8 +9,9 @@ member_no number primary key,
 dang_no references dang(dang_no) on delete cascade,
 user_no references dang_user(user_no) on delete cascade,
 member_nick varchar2(30) not null,
+member_message varchar2(180),
 member_owner char(1) default 'N' check(member_owner in('Y','N')) not null,
 member_grade varchar2(4) default 'LV.1' check(member_grade in('LV.1', 'LV.2', 'LV.3', 'LV.4', 'LV.5')) not null,
-member_score number default 0 not null,
+member_score number default 0 check(member_score >= 0) not null,
 member_joindate date default sysdate not null
 );
