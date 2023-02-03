c538039374b852bfddf51f83b0d909016963f9a5
Luverduck <109222367+Luverduck@users.noreply.github.com>
2023-02-03 14:42:38 +0900

    회원 번호 on delete cascade 추가

--- Database/dang_interest.sql
+++ Database/dang_interest.sql
@@ -1,5 +1,5 @@
 -- 회원 관심지역(dang_interest)
 create table dang_interest(
-user_no references dang_user(user_no),
+user_no references dang_user(user_no) on delete cascade,
 interest_area varchar2(30) not null
 );
