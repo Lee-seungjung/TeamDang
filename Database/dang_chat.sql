14dc112cc3d7ef98232f8f82d0d1fc94e5ee48c9
Luverduck <109222367+Luverduck@users.noreply.github.com>
2023-02-03 17:09:05 +0900

    채팅 내용 타입에 check 조건 추가

--- Database/dang_chat.sql
+++ Database/dang_chat.sql
@@ -10,7 +10,7 @@ chat_no number primary key,
 room_no references room(room_no) on delete cascade,
 user_no number not null,
 member_nick varchar2(30) not null,
-chat_type varchar2(5) default 'text' not null,
+chat_type varchar2(5) check(chat_type in ('text' ,'image')) not null,
 chat_content varchar2(3000) not null,
 chat_date date default sysdate not null,
 chat_status number default 1 not null
